92da68085f93cab8fadfc78de0878e1ab097e874
Jacqueline Shepherd <47365296+jacqueline-weiland@users.noreply.github.com>
2022-12-01 10:07:32 -0500

    Create day_1.py

---
+++ day_1.py
@@ -0,0 +1,29 @@
+#Open Up File
+with open('day_1_input.txt') as f:
+    lines = f.readlines()
+
+#PART 1
+
+aDict = {}
+counter = 0
+for each in lines:
+    if each.strip(): #if the line has a value
+        try:
+            aDict[counter] += int(each) #it's an additional fruit for an elf so we want to add it to the elf's total
+        except: 
+            aDict[counter] = int(each) #it's a new elf so we're starting that elf with their calorie count
+    else: #if the line is empty
+        counter += 1 #move on to next elf
+print(aDict)
+print(max(aDict.values()))
+
+#PART 2
+
+aList = []
+for each in aDict.values():
+    aList.append(each)
+    
+aList = sorted(aList)
+print(aList)
+
+print(sum(aList[-3:]))
